5e2abf4ca1328cc21e4e50264a0579f6686fbc7a
YUMMI-X-REBOT <132044404+YUMMI-X-REBOT@users.noreply.github.com>
2023-05-29 22:01:00 +0530

    Update config.py

--- SankiMusic/utilities/config/config.py
+++ SankiMusic/utilities/config/config.py
@@ -64,7 +64,7 @@ clean = {}
 autoclean = []
 ############################
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/83cfcc36ef441e516d28f.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/af616e9542a0999f4fa84.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/6abc32c3858f205434cd5.jpg")
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
